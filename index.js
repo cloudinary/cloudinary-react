8ed319f642876428c1b6c62f23b4f1023db167f7
Amir Tocker <amir.tocker@cloudinary.com>
2016-09-28 11:25:17 +0300

    Move index.js to the src folder

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import CloudinaryContext from './src/components/CloudinaryContext';
-import Image from './src/components/Image';
-import Transformation from './src/components/Transformation';
-import Video from './src/components/Video';
-
-export  { CloudinaryContext, Image, Transformation, Video};
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import CloudinaryContext from './components/CloudinaryContext';
+import Image from './components/Image';
+import Transformation from './components/Transformation';
+import Video from './components/Video';
+
+export  { CloudinaryContext, Image, Transformation, Video};
