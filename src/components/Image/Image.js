bdca3ffd446f2a4165c07bfae9d8cb664d92b658
Amir Tocker <amir.tocker@cloudinary.com>
2016-12-15 12:08:43 +0200

    Step 3 - Support all parameters

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,20 +1,56 @@
 import React, {Component, PropTypes} from 'react';
 import cloudinary from 'cloudinary-core';
 
+const camelCase = cloudinary.Util.camelCase;
+const snakeCase = cloudinary.Util.snakeCase;
+
+
 export default class Image extends React.Component {
   render() {
-    let cl = cloudinary.Cloudinary.new({cloud_name: this.props.cloudName});
-    let {publicId, width, height} = this.props;
-    let url = cl.url(publicId, {width, height, crop: "scale"});
+    let {public_id, ...options} = getOptions(this.props);
+    let cl = cloudinary.Cloudinary.new(options);
+    let transformation = cloudinary.Transformation.new(options);
+    let url = cl.url(public_id, transformation);
+    let attributes = transformation.toHtmlAttributes();
     return (
-      <img src={url} />
+      <img {...attributes} src={url} />
     );
   }
 }
 
-Image.propTypes = {
-  cloudName: PropTypes.string.isRequired,
-  publicId: PropTypes.string.isRequired,
-  width: PropTypes.string,
-  height: PropTypes.string
-};
+let allParams = cloudinary.Configuration.CONFIG_PARAMS.concat(cloudinary.Transformation.PARAM_NAMES);
+Image.VALID_OPTIONS = allParams.map( camelCase);
+Image.propTypes = typesFrom(Image.VALID_OPTIONS);
+
+/**
+ * Create a React type definition object. All items are PropTypes.string.
+ * @param {Array} configparams a list of parameter names
+ * @returns {Object}
+ */
+function typesFrom(configparams) {
+  configparams = configparams || [];
+  const types = {};
+  for (let key of configparams) {
+    types[camelCase(key)] = PropTypes.string;
+  }
+  return types;
+}
+
+/**
+ * Combine props and context to create an option Object that can be passed to Cloudinary methods.<br>
+ *   All names are converted to snake_case.
+ * @param props
+ * @param context
+ * @returns {{}}
+ */
+function getOptions(props) {
+  var options = {};
+
+  for(let key in props) {
+    let value = props[key];
+    if(value !== null && value !== undefined) {
+      options[snakeCase(key)] = value;
+    }
+  }
+  return options;
+}
