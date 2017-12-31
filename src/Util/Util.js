b351d411d92e154b965720e7911698d9a2b60891
DanaShalev <34619711+DanaShalev@users.noreply.github.com>
2017-12-31 14:41:31 +0200

    github-fixes-#31-error-maximum-call-stack-exceeded (#39)
    * github-fixes-#31-error-maximum-call-stack-exceeded
    
    * remove equals function since it does not work with cyclic objects and it is not used anymore

--- src/Util/Util.js
+++ src/Util/Util.js
@@ -2,5 +2,4 @@ export debounce from './debounce';
 export firstDefined from './firstDefined';
 export closestAbove from './closestAbove';
 export {requestAnimationFrame, cancelAnimationFrame } from './requestAnimationFrame';
-export equals from './equals';
 export isElement from './isElement';
