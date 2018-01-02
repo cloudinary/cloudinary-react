7a8db3d2aab76634bbb7fe4ffdcb6263327756c2
Eitan Peer <eitan.peer@cloudinary.com>
2018-01-02 16:50:02 +0200

    Filter Cloudinary custom props from rendered div (#37)
    * Filter Cloudinary custom props from rendered div
    
    When rendering the div underlying CloudinaryContext remove Cloudinary custom props to avoid errors such as "React does not recognize the `cloudName` prop on a DOM element"
    
    * Restore single quote import style, add case to unit test
    
    * Fix test after merge with React 16

--- test/ContextTest.js
+++ test/ContextTest.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
+import { mount } from 'enzyme';
 import Image from '../src/components/Image';
 import Transformation from '../src/components/Transformation';
 import CloudinaryContext from '../src/components/CloudinaryContext';
 
-
 describe('CloudinaryContext', () => {
-  beforeEach(() => {
-  });
   it("should pass properties to children", function() {
     let tag = mount(
       <CloudinaryContext className="root" cloudName="demo">
-        <Image publicId="sample"  />
+        <Image publicId="sample" />
       </CloudinaryContext>
     );
 
@@ -23,16 +20,43 @@ describe('CloudinaryContext', () => {
     expect(img.instance().state.url).to.equal("http://res.cloudinary.com/demo/image/upload/sample");
   });
 
+  it("should remove Cloudinary custom properties from CloudinaryContext component", function() {
+    let html = mount(
+      <CloudinaryContext
+        className="root"
+        cloudName="demo"
+        quality="auto"
+        secure="true"
+        role="tab"
+        aria-live="polite"
+      >
+        <Image publicId="sample" />
+      </CloudinaryContext>
+    );
+
+    const contextDiv = html.find("div");
+    expect(contextDiv.find(".root").length).to.equal(1);
+    expect(contextDiv.find("[role='tab']").length).to.equal(1);
+    expect(contextDiv.find("[aria-live='polite']").length).to.equal(1);
+    expect(contextDiv.find("[cloudName='demo']").length).to.equal(0);
+    expect(contextDiv.find("[quality]").length).to.equal(0);
+
+    // Verify that transformations from context are applied to components
+    expect(contextDiv.find('img').prop("src")).to.equal("https://res.cloudinary.com/demo/image/upload/q_auto/sample");
+  });
+
   it("should allow chained Contexts", function() {
     let tag = mount(
       <CloudinaryContext cloudName="demo">
         <CloudinaryContext width="100" crop="scale">
-          <Image publicId="sample"/>
+          <Image publicId="sample" />
         </CloudinaryContext>
       </CloudinaryContext>
     );
-    expect(tag.containsMatchingElement(
-      <img src="http://res.cloudinary.com/demo/image/upload/c_scale,w_100/sample"/>
-    )).to.equal(true);
+    expect(
+      tag.containsMatchingElement(
+        <img src="http://res.cloudinary.com/demo/image/upload/c_scale,w_100/sample" />
+      )
+    ).to.equal(true);
   });
 });
