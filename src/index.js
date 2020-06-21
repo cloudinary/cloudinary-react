e1943071c3a71b7cc2bfa1688ddf9cc18fc7ef3a
maoznir <nir.maoz@cloudinary.com>
2020-06-21 18:10:18 +0300

    Fix Audio component export

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import CloudinaryContext from './components/CloudinaryContext';
 import Image from './components/Image';
 import Transformation from './components/Transformation';
 import Video from './components/Video';
+import Audio from './components/Audio';
 
-export  { CloudinaryContext, Image, Transformation, Video};
+export  { CloudinaryContext, Image, Transformation, Video, Audio};
