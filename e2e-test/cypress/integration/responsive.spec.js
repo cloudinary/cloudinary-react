2e69f3488682f94e1de90e9b8f8d532296d21ffa
maoznir <nir.maoz@cloudinary.com>
2020-07-11 07:11:42 +0300

    Add responsive test description

--- e2e-test/cypress/integration/responsive.spec.js
+++ e2e-test/cypress/integration/responsive.spec.js
@@ -1,10 +1,18 @@
+/**
+ * When 'responsive' is passed to the Image component, cloudinary-core generates a data-src attribute instead of src.
+ * Then cloudinary.responsive() is called, it will add a 'src' attribute with calculated width according to the size
+ * of the <img> element's container.
+ * In these tests we first test for existence of 'data-src' attribute, and then we check that 'src' was also generated.
+ * Remember that Cypress removes the need to add a wait or timeout, and waits for a test to be true until it's default
+ * timeout is reached.
+ */
 describe('Responsive Image', () => {
   beforeEach(() => {
     // runs before each test in the block
     cy.visit('/');
     cy.get('#responsiveBtn').click();          // Click on button
   });
-  it('Responsive Image Override', () => {
+  it('Should generate transformation with specified width value', () => {
     cy.get('#responsive-override')
       .should('have.attr', 'data-src').should('equal','http://res.cloudinary.com/demo/image/upload/c_scale,w_100/sample')
     cy.get('#responsive-override')
