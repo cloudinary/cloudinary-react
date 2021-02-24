2d37cc1a1f8552426ae1ee9bc4080c0798d08309
maoznir <nir.maoz@cloudinary.com>
2021-02-24 12:42:07 +0200

    Remove duplicate property definition in setupTests.js

--- __tests__/setupTests.js
+++ __tests__/setupTests.js
@@ -48,11 +48,6 @@ function setupIntersectionObserverMock({
     observe = observe
     unobserve = unobserve
   }
-  Object.defineProperty(window, 'IntersectionObserver', {
-    writable: true,
-    configurable: true,
-    value: IntersectionObserver
-  })
   Object.defineProperty(global, 'IntersectionObserver', {
     writable: true,
     configurable: true,
