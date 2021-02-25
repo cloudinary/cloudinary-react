98f0758756519e7e36c41439bac10184a7d418db
maoznir <nir.maoz@cloudinary.com>
2021-02-25 15:21:17 +0200

    Update storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
