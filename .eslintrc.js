d2f0305e86f6af4d92e1d340771dae714c29f192
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2021-02-26 22:47:07 +0200

    Add eslint (#219)

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['standard', 'standard-react'],
+  env: {
+    node: true,
+    jest: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      legacyDecorators: true,
+      jsx: true
+    }
+  },
+  settings: {
+    react: { version: '16' }
+  },
+  rules: {
+    'space-before-function-paren': 0,
+    'react/prop-types': 0,
+    'react/jsx-handler-names': 0,
+    'react/jsx-fragments': 0,
+    'react/no-unused-prop-types': 0,
+    'import/export': 0,
+    'no-unused-vars': 'off',
+    semi: [1, 'always']
+  }
+};
