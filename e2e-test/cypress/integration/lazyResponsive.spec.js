f6c900b52248e47b7a034a3c984e99e2fc19b833
nir maoz <nir.maoz@cloudinary.com>
2022-07-17 13:50:06 +0300

    chore: update dependencies

--- e2e-test/cypress/integration/lazyResponsive.spec.js
+++ e2e-test/cypress/integration/lazyResponsive.spec.js
@@ -12,7 +12,20 @@ describe('Lazy Responsive Image', () => {
   it('Should have src attribute when view', () => {
     cy.scrollTo(0, 3000);
     cy.get('#lazyResponsive')
-      .should('be.visible')
-      .should('have.attr', 'src').should('equal', 'http://res.cloudinary.com/demo/image/upload/c_scale,w_400/sample');
+      .should('have.attr', 'src').should('equal', 'http://res.cloudinary.com/demo/image/upload/c_scale,w_400/sample')
+  });
+  it('Should have width when in view', () => {
+    cy.scrollTo(0, 3000);
+    cy.get('#lazyResponsive')
+      .should('have.css', 'width')
+    cy.get('#lazyResponsive').invoke('outerWidth')
+      .should('be.gt', 0);
+  });
+  it('Should have height when in view', () => {
+    cy.scrollTo(0, 3000);
+    cy.get('#lazyResponsive')
+      .should('have.css', 'height')
+    cy.get('#lazyResponsive').invoke('outerHeight')
+      .should('be.gt', 0);
   });
 });
